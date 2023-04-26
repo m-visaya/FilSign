6aab5367f96a06cd8dc2bb3df0e411ed997b0c9b
Marco Visaya <marco.visaya527@gmail.com>
2023-04-26 20:25:52 +0800

    Update class names, add yolom model

--- model/main.py
+++ model/main.py
@@ -9,57 +9,17 @@ model = YOLO('./model.onnx')
 
 app = FastAPI()
 
-names = [
-    "A",
-    "B",
-    "Boss",
-    "C",
-    "D",
-    "E",
-    "F",
-    "Father",
-    "G",
-    "Good",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "Me",
-    "Mine",
-    "Mother",
-    "N",
-    "O",
-    "Onion",
-    "P",
-    "Q",
-    "Quiet",
-    "R",
-    "Responsible",
-    "S",
-    "Serious",
-    "T",
-    "Think",
-    "This",
-    "U",
-    "V",
-    "W",
-    "Wait",
-    "Water",
-    "X",
-    "Y",
-    "You",
-    "Z",
-  ]
+names = ['A', 'B', 'Boss', 'C', 'D', 'E', 'F', 'Father', 'G', 'Good', 'H', 'I', 'I love you', 'J', 'K', 'L', 'M', 'Me', 'Mine',
+         'Mother', 'N', 'O', 'P', 'Q', 'Quiet', 'R', 'S', 'Serious', 'T', 'Think', 'This', 'U', 'V', 'W', 'Wait', 'Water', 'X', 'Y', 'You', 'Z']
 
 
 @app.get("/")
 def index():
     return "Filsign API working"
 
+
 @app.post("/predict")
-async def get_tensor(file: Request):
+async def predict(file: Request):
     raw_body = await file.body()
     image_data = json.loads(raw_body)
     buffer = base64.b64decode(image_data['image'])
@@ -67,8 +27,8 @@ async def get_tensor(file: Request):
     img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
     results = model.predict(img, imgsz=640)
     classes = results[0].boxes.cls
-    
-    name = names[int(classes[0])] if len(classes) > 0  else ""
-    print("class",name)
+
+    name = names[int(classes[0])] if len(classes) > 0 else ""
+    print("class", name)
 
     return {"class": name}
